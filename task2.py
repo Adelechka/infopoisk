0141bd174b16ec93e70c46f2def2a7803566e9dd
adelechka <h8mfru6r>
2023-03-09 23:28:59 +0300

    task2 updated

--- task2.py
+++ task2.py
@@ -22,17 +22,23 @@ if __name__ == '__main__':
     stopwords = stopwords.words("russian")
     lemmatizer = pymorphy2.MorphAnalyzer(lang='ru')
 
-    tokens = list()
-    lemmas = {}
+    tokens_all = set()
+    lemmas_all = {}
+    counter = 0
 
-    for file in files:
-        html = codecs.open(file, 'r', encoding='cp1251')
+    for i in range(100):
+        print(i)
+        tokens = list()
+        html = codecs.open(output_task1 + str(i) + '.txt', 'r', encoding='cp1251')
         try:
             data = html.read()
             text = BeautifulSoup(data, 'html.parser').get_text().lower()
             tokens += nltk.word_tokenize(text)
+
         except:
             continue
+
+        lemmas = {}
 
         cleaning1 = set()
         for word in tokens:
@@ -50,22 +56,33 @@ if __name__ == '__main__':
             except:
                 continue
 
+        for token in cleaning2:
+            lemma = lemmatizer.parse(token)[0].normal_form
+            if lemmas.keys().__contains__(lemma):
+                lemmas[lemma] += [token]
+            else:
+                lemmas[lemma] = [token]
+
+        lemmas_all.update(lemmas)
+        tokens_all.update(cleaning2)
+        lemmas_file = open(output_task2 + 'lemmas' + str(i) + '.txt', 'w')
+        for lemma in lemmas:
+            tokens = ' '
+            for token in lemmas[lemma]:
+                tokens += ' ' + token
+            lemmas_file.write(lemma + ":" + tokens + '\n')
+        lemmas_file.close()
+
+
     tokens_file = open(output_task2 + 'tokens.txt', 'w')
-    for token in list(cleaning2):
+    for token in tokens_all:
         tokens_file.write(token + "\n")
     tokens_file.close()
 
-    for token in cleaning2:
-        lemma = lemmatizer.parse(token)[0].normal_form
-        if lemmas.keys().__contains__(lemma):
-            lemmas[lemma] += [token]
-        else:
-            lemmas[lemma] = [token]
-
-    lemmas_file = open(output_task2 + 'lemmas.txt', 'w')
-    for lemma in lemmas:
+    lemmas_all_file = open(output_task2 + 'lemmas.txt', 'w')
+    for lemma in lemmas_all:
         tokens = ' '
-        for token in lemmas[lemma]:
+        for token in lemmas_all[lemma]:
             tokens += ' ' + token
-        lemmas_file.write(lemma + ":" + tokens + '\n')
-    lemmas_file.close()
+        lemmas_all_file.write(lemma + ":" + tokens + '\n')
+    lemmas_all_file.close()
